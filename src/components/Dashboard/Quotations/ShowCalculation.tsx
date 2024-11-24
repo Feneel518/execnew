69e23792c80d3883cb508cdde2bf3acad7d6fec5
feneel <feneelp@gmail.com>
2024-11-24 17:19:47 +0530

    Fixed naviagtion issues

--- src/components/Dashboard/Quotations/ShowCalculation.tsx
+++ src/components/Dashboard/Quotations/ShowCalculation.tsx
@@ -26,10 +26,16 @@ const ShowCalculation: FC<ShowCalculationProps> = ({ quotationTotal }) => {
       </div>
       {show && (
         <div className="text-white flex flex-col gap-4 transition-all duration-300 ease-in-out">
+          <div className="grid grid-cols-4 gap-8">
+            <div className="">Item</div>
+            <div className="">Net Total</div>
+            <div className="">GST Total</div>
+            <div className="">Discount Amount</div>
+          </div>
           {discounts.map((disc) => {
             return (
               <>
-                <div className="grid grid-cols-3 gap-8">
+                <div className="grid grid-cols-4 gap-8">
                   <div className="">
                     {disc === 0 ? "Quotation total" : `${disc}% discount`}
                   </div>
@@ -45,6 +51,13 @@ const ShowCalculation: FC<ShowCalculationProps> = ({ quotationTotal }) => {
                       quotationTotal *
                         (disc === 0 ? 100 / 100 : (100 - disc) / 100) *
                         1.18,
+                      true
+                    )}
+                  </div>
+                  <div className="">
+                    {formatPrice(
+                      quotationTotal * (disc === 0 ? 0 / 100 : disc / 100),
+
                       true
                     )}
                   </div>
